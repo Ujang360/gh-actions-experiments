35063586f4e02341769eacbd71e42825db12dbc2
Aditya Kresna <aditya.kresna@outlook.co.id>
2021-04-15 20:14:20 +0700

    feat: add functional hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+pub fn hello(to: &str) -> String {
+    format!("Hello, {}!", to)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hello_to_is_as_expected() {
+        let to = "Kresna";
+        let expected_result = format!("Hello, {}!", to);
+
+        assert_eq!(hello(to), expected_result);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hello("world!"));
 }
